627f883ac6ba4bf81aceb58f6de93e07407ec5ef
Ievgenii <zajazz911@gmail.com>
2023-08-25 00:33:07 +0300

    add models to both apps

--- shop/shop/models.py
+++ shop/shop/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+STATUS = (
+    (0, "cart"),
+    (1, "ordered"),
+    (2, "success"),
+    (3, "fail"),
+)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.DecimalField()
+    quantity = models.IntegerField()
+    id_in_store = models.IntegerField()
+
+
+class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
+    delivery_address = models.CharField(max_length=140)
+
+
+class OrderItem(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(default=1)
+    product = models.ForeignKey(Book, unique=False, on_delete=models.PROTECT)
+    ordered = models.BooleanField(default=False)
--- warehouse/warehouse/models.py
+++ warehouse/warehouse/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+PLACE = (
+    (0, "in_the_warehouse"),
+    (1, "on_the_shelf"),
+)
+
+STATUS = (
+    (0, "in_work"),
+    (1, "success"),
+    (2, "fail"),
+)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.DecimalField()
+
+
+class BookItem(models.Model):
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    place = models.IntegerField(choices=PLACE, default=0)
+
+
+class Order(models.Model):
+    user_email = models.EmailField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    delivery_address = models.CharField(max_length=200)
+    order_id_in_shop = models.IntegerField()
+
+
+class OrderItem(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    warehouse_book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+class BookItemOrderItem(models.Model):
+    order_item_id = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
+    book_item_id = models.ForeignKey(BookItem, on_delete=models.CASCADE)
